3788ca8272fad39a2ac3fb16f2fb553eaf4c4fc5
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-20 01:58:03 +0200

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) return [];
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((newGrade) => newGrade.studentId === student.id);
+      return {
+        ...student,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
+}
