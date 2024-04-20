ba06137d066b8a53c29e340a8ecae79f8cf50efe
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-04-20 02:34:39 +0200

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !startString instanceof String) return '';
+  if (!set || !startString || typeof startString !== 'string') return '';
   return [...set].filter((x) => x && x.startsWith(startString)).map((x) => x.slice(startString.length)).join('-');
 }
