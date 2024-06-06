8c6d444c098ef7d2941d036537ab6f5adc5b6e32
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-06 11:37:30 +0300

    nodejs

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert
+const calculateNumber = require('./0-calcul')
+
+describe('calcul', () => {
+  it('should return 5', () => {
+    const result = calculateNumber(3.4, 2)
+    assert.equal(result, 5)
+  })
+  it('should return 6', () => {
+    const result = calculateNumber(3, 3.1)
+    assert.equal(result, 6)
+  })
+  it('should return 7', () => {
+    const result = calculateNumber(3.6, 3)
+    assert.equal(result, 7)
+  })
+  it('should return 8', () => {
+    const result = calculateNumber(4, 3.9)
+    assert.equal(result, 8)
+  })
+  it('should return number', () => {
+    const result = calculateNumber(3.6, 2)
+    assert.typeOf(result, 'number')
+  })
+})
