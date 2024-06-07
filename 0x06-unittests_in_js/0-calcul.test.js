731594745382771d0bcfc1d02fa562c9971d6a8f
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-07 04:04:28 +0300

    nodejs

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,43 @@ const assert = require('assert')
 const calculateNumber = require('./0-calcul')
 
 describe('calcul', () => {
-  it('should return 5', () => {
+  it('should return -4 for -2.4, -2', () => {
+    const result = calculateNumber(-2.4, -2)
+    assert.equal(result, -4)
+  })
+  it('should return -3 for 0, -3.3', () => {
+    const result = calculateNumber(0, -3.3)
+    assert.equal(result, -3)
+  })
+  it('should return -2 for -1, -1', () => {
+    const result = calculateNumber(-1, -1)
+    assert.equal(result, -2)
+  })
+  it('should return -1 for -7, 6', () => {
+    const result = calculateNumber(-7, 6)
+    assert.equal(result, -1)
+  })
+  it('should return 0 for -1, 0.9', () => {
+    const result = calculateNumber(-1, 0.9)
+    assert.equal(result, 0)
+  })
+  it('should return 0 for 0, 0.1', () => {
+    const result = calculateNumber(0, 0.1)
+    assert.equal(result, 0)
+  })
+  it('should return 5 for 3.4, 2', () => {
     const result = calculateNumber(3.4, 2)
     assert.equal(result, 5)
   })
-  it('should return 6', () => {
+  it('should return 6 for 3, 3.1', () => {
     const result = calculateNumber(3, 3.1)
     assert.equal(result, 6)
   })
-  it('should return 7', () => {
+  it('should return 7 for 3.6, 3', () => {
     const result = calculateNumber(3.6, 3)
     assert.equal(result, 7)
   })
-  it('should return 8', () => {
+  it('should return 8 for 4, 3.9', () => {
     const result = calculateNumber(4, 3.9)
     assert.equal(result, 8)
   })
