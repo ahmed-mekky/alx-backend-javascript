c7a693f99db1379f64e5480c27621c92ab0625ba
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-07 05:07:11 +0300

    i hate this node_modules thing

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+function calculateNumber(type, a, b) {
+  if (typeof a != 'number' || typeof b != 'number') {
+    return null
+  }
+  if (type == 'SUM') {
+    return Math.round(a) + Math.round(b)
+  }
+  else if (type == 'SUBTRACT') {
+    return Math.round(a) - Math.round(b)
+  }
+  else if (type == 'DIVIDE') {
+    return Math.round(a) / Math.round(b)
+  }
+  else {
+    return null
+  }
+}
+
+module.exports = calculateNumber
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+const assert = require('assert')
+const calculateNumber = require('./0-calcul.js')
+
+describe('calculateNumber', () => {
+  it('should return number', () => {
+    const result = calculateNumber(3.6, 2)
+    assert.equal(typeof result, 'number')
+  })
+  it('should retuen null if not type SUM or SUBTRACT or DIVIDE', () => {
+    const result = calculateNumber('alx', 1, 1.6)
+    assert.equal(result, null)
+  })
+  it('SUM', () => {
+    const result = calculateNumber('SUM', -1, 0.9)
+    assert.equal(result, 0)
+  })
+  it('SUBTRACT', () => {
+    const result = calculateNumber('SUBTRACT', 9.1, 2)
+    assert.equal(result, 7)
+  })
+  it(' DIVIDE ', () => {
+    const result = calculateNumber('DIVIDE', 5.7, 2)
+    assert.equal(result, 3)
+  })
+  it('should not accept string a', () => {
+    const result = calculateNumber('SUM', 1, 'alx')
+    assert.equal(result, null)
+  })
+  it('should not accept string b', () => {
+    const result = calculateNumber('SUM', 'alx', 2)
+    assert.equal(result, null)
+  })
+})
