62d0e6e41b29a33ba92f5489c3ac7249b64e9e56
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-07 04:45:21 +0300

    nodejs

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,8 @@
+import cleanSet from './8-clean_set.js';
+
+console.log(
+  cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), "23")
+);
+console.log(
+  cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), '')
+);
