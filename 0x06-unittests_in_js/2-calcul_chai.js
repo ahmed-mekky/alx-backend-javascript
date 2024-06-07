e80523d451533b19adbc55a0928a0ff2093c01c5
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-07 05:26:45 +0300

    nodejs

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+function calculateNumber(type, a, b) {
+  if (typeof a != 'number' || typeof b != 'number') {
+    return null
+  }
+  if (type == 'SUM') {
+    return Math.round(a) + Math.round(b)
+  }
+  else if (type == 'SUBTRACT') {
+    return Math.round(a) - Math.round(b)
+  }
+  else if (type == 'DIVIDE') {
+    if (b == 0) {
+        return null
+    }
+    return Math.round(a) / Math.round(b)
+  }
+  else {
+    return null
+  }
+}
+
+module.exports = calculateNumber
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+const expect = require('chai').expect
+const calculateNumber = require('./0-calcul.js')
+
+describe('calculateNumber', () => {
+  it('should return number', () => {
+    const result = calculateNumber(3.6, 2)
+    expect(result).to.be.a('number')
+  })
+  it('should retuen null if not type SUM or SUBTRACT or DIVIDE', () => {
+    const result = calculateNumber('alx', 1, 1.6)
+    expect(result).to.be(null)
+  })
+  it('SUM', () => {
+    const result = calculateNumber('SUM', -1, 0.9)
+    expect(result).to.be(0)
+  })
+  it('SUBTRACT', () => {
+    const result = calculateNumber('SUBTRACT', 9.1, 2)
+    expect(result).to.be(7)
+  })
+  it(' DIVIDE ', () => {
+    const result = calculateNumber('DIVIDE', 5.7, 2)
+    expect(result).to.be(3)
+  })
+  it(' DIVIDE null if b is 0', () => {
+    const result = calculateNumber('DIVIDE', 5.7, 0)
+    expect(result).to.be(null)
+  })
+  it('should not accept string a', () => {
+    const result = calculateNumber('SUM', 1, 'alx')
+    expect(result).to.be(null)
+  })
+  it('should not accept string b', () => {
+    const result = calculateNumber('SUM', 'alx', 2)
+    expect(result).to.be(null)
+  })
+})
